728ec63192ff03bcbf4b81cc67fa68a8bebb5e11
vebjornkjus <vebjornfk@uia.no>
2025-10-25 12:28:53 +0200

    styling to everything, still needs fixing

--- public/chat/new.php
+++ public/chat/new.php
@@ -1,15 +1,19 @@
 <?php 
 declare(strict_types=1);
 
+// Load dependencies
 require_once __DIR - '/../../src/auth.php';
 require_once __DIR - '/../../src/conversations.php';
 
+// Require users to be logged in to create new chat
 Auth::requireLogin();
 $userId = Auth::userId();
 
+// Creates new conversation in database by using Conversations class in conversations.php
 $conversationId = Conversations::create($userId);
 
-header("Location: /chatciew.php?id={conversationId}");
+// Redirects users to view.php when starting a new chat
+header("Location: /chat/view.php?id={$conversationId}");
 exit;
 
 ?>
