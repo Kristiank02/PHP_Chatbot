bf002c277a69d6367cfa30472d9a1fd7e5c9cba0
vebjornkjus <vebjornfk@uia.no>
2025-11-24 19:18:34 +0100

    Edit to dashboard title

--- public/admin/dashboard.php
+++ public/admin/dashboard.php
@@ -116,7 +116,7 @@ $currentUser = auth::getCurrentUser();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard - Modul 7</title>
+    <title>Admin Dashboard</title>
     <link rel="stylesheet" href="../assets/css/main-compiled.css?v=<?= time() ?>">
     <link rel="stylesheet" href="../assets/css/admin.css?v=<?= time() ?>">
 </head>
